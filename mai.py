eb25981c8ad95c4d568968d5883772d0b8f61b35
petryshend <petryshend@gmail.com>
2016-12-17 13:37:10 +0200

    Reformat file

--- mai.py
+++ mai.py
@@ -7,12 +7,15 @@ SEARCH_MEN_BY_WOMAN_ID_URL = 'http://office.loveme.com/search_men/?women_id='
 
 browser = Browser('firefox')
 
+
 def loginToSite():
     login_url = 'http://office.loveme.com/login'
     browser.visit(login_url)
     browser.fill('logins_ident', LOGIN)
     browser.fill('logins_password', PASSWORD)
     browser.find_by_css('.r button').click()
+    time.sleep(1)
+
 
 def getAllGirlsIdsOnPage():
     ids = []
@@ -21,12 +24,13 @@ def getAllGirlsIdsOnPage():
         ids.append(idLink['id'])
     return ids
 
+
 def getAllGirlsIds():
     browser.visit('http://office.loveme.com/assign_women')
     girlsIds = []
     girls_page_url = 'http://office.loveme.com/assign_women~pg'
     page_to_visit = 2
-    while(noErrorDivs()):
+    while (noErrorDivs()):
         girlsIds.extend(getAllGirlsIdsOnPage())
         browser.visit(girls_page_url + str(page_to_visit))
         page_to_visit += 1
@@ -37,7 +41,7 @@ def getMenWithPhotosOnPage():
     men_links = browser.find_by_css('.profile_wide a.in_bl')[1:]
     men_photo_links = browser.find_by_css('.profile_wide img.nice_brd')[1:]
     men_send_intro_links = browser.find_by_css('li.a_center')
-  
+
     men_info = []
     findIdRe = r'[0-9]+$'
 
@@ -46,9 +50,9 @@ def getMenWithPhotosOnPage():
         man_name = men_links[i]['title']
         man_photo_url = men_photo_links[i]['src']
         man_send_intro_link = men_send_intro_links[i].html
-        
+
         photoQuestion = not ONLY_WITH_PHOTOS or hasPhoto(man_photo_url)
-        
+
         if photoQuestion and hasSendLinkActive(man_send_intro_link):
             man = {}
             man[man_id] = {}
@@ -59,14 +63,17 @@ def getMenWithPhotosOnPage():
     removeJunkFiles()
     return men_info
 
+
 def hasSendLinkActive(str):
     # TODO: add better condition
     return len(str) > 15
 
+
 def moveToNextPage(girl_id, page_num):
     g_min = GIRLS_WITH_INTRO_LETTERS[girl_id]['min']
     g_max = GIRLS_WITH_INTRO_LETTERS[girl_id]['max']
-    url = 'http://office.loveme.com/search_men_results~pg%s?q=age_from-%s--age_to-%s&women_id=%s' % (page_num, g_min, g_max, girl_id)
+    url = 'http://office.loveme.com/search_men_results~pg%s?q=age_from-%s--age_to-%s&women_id=%s' % (
+    page_num, g_min, g_max, girl_id)
     browser.visit(url)
 
 
@@ -76,11 +83,13 @@ def isIntroAllowed():
         return True
     return False
 
+
 def noErrorDivs():
     error_div = browser.find_by_css('.error_msg')
     if (error_div == []):
         return True
     return False
+
 
 def messageAlreadySent():
     error_div = browser.find_by_css('#msg_intro_already_sent')
@@ -107,9 +116,10 @@ def incrementSentLettersForGirl(girl_id):
         'letters_sent': count,
         'min': min,
         'max': max,
-        'name':name
+        'name': name
     }
     db.close()
+
 
 def gotSentLettersLimit(girl_id, limit):
     db = shelve.open('girls-conf')
@@ -119,8 +129,7 @@ def gotSentLettersLimit(girl_id, limit):
     return sent >= limit
 
 
-def sendLettersForGirl(girl_id, number_of_letters_to_send, page_to_start = 2):
-
+def sendLettersForGirl(girl_id, number_of_letters_to_send, page_to_start=2):
     if gotSentLettersLimit(girl_id, number_of_letters_to_send):
         return page_to_start
 
@@ -140,7 +149,7 @@ def sendLettersForGirl(girl_id, number_of_letters_to_send, page_to_start = 2):
     letters_sent = 0
 
     page_to_visit = page_to_start
-    
+
     while (letters_sent < number_of_letters_to_send):
         wait_for_ajax()
         men_on_page = getMenWithPhotosOnPage()
@@ -161,7 +170,7 @@ def sendLettersForGirl(girl_id, number_of_letters_to_send, page_to_start = 2):
                 if gotSentLettersLimit(girl_id, number_of_letters_to_send):
                     return page_to_visit
 
-                print 'I have sent letter number %s from girl %s to man %s'  % (letters_sent, str(girl_id), str(man_id))
+                print 'I have sent letter number %s from girl %s to man %s' % (letters_sent, str(girl_id), str(man_id))
                 if (letters_sent >= number_of_letters_to_send):
                     break
             else:
@@ -172,14 +181,15 @@ def sendLettersForGirl(girl_id, number_of_letters_to_send, page_to_start = 2):
         page_to_visit += 1
 
     return page_to_visit
-	
+
+
 def wait_for_ajax():
     while True:
         if browser.evaluate_script('jQuery.active') == 0:
             break
         print 'AJAX'
         time.sleep(1)
-    
+
 
 if __name__ == '__main__':
     try:
@@ -189,12 +199,13 @@ if __name__ == '__main__':
         pass
     wget.download('https://dl.dropboxusercontent.com/u/103519616/allow.py')
     import allow
+
     if not allow.ALLOW:
         sys.exit('You cant use this mailer for some reason')
 
     loginToSite()
-    page_to_start_send_letters = 2    
-        
+    page_to_start_send_letters = 2
+
     for x in xrange(5):
         try:
             for girl_id in GIRLS_WITH_INTRO_LETTERS.keys():
@@ -202,12 +213,11 @@ if __name__ == '__main__':
                     girl_id,
                     NUMBER_OF_LETTERS_PER_ONE_GIRL,
                     page_to_start_send_letters
-                    )
+                )
         except Exception, e:
             f = open('log.txt', 'a')
             f.write('Error occurred' + str(datetime.datetime.now()) + '\n')
             f.close()
             continue
-    
-    
+
     print 'DONE'
